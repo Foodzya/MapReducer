991e522dedcff3526e4a46c3bfeeac19006076cf
Foodzya <afodzya@gmail.com>
2021-07-20 08:45:31 +0300

    Changes are made

--- MapReducer/Program.cs
+++ MapReducer/Program.cs
@@ -13,8 +13,6 @@ namespace MapReducer
             List<int> testList = new List<int> { 5, 6, 5, 6, 1230, 50, 100, 50, -5, 50, 99 };
             List<string> listOfNames = new List<string> { "Alex", "Peter", "Andrew", "Axel", "Boris", "Ashton", "John", "David", "Bob", "Jack", "Owshen", "Hobs" };
 
-            int[] vs = new int[10];
-
             listOfNames.MapReduce(s => s + " ", s => s.First(), (s, m) => s + m);
             testList.MapReduce(s => s, s => s, (s, m) => s + m);
 
